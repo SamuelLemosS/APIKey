29fb15c4689309f6326ec473597d9de6278ecb72
SamuelLemosS <107142956+SamuelLemosS@users.noreply.github.com>
2022-09-26 14:54:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const h3 = document.getElementById("h3");
 
 const gerarEspecie = async() =>{
     const apikey=('9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee')
-    const response = await fetch(`http://apiv3.iucnredlist.org/api/v3/species/page/0?token=${apikey}`);
+    const response = await fetch(`https://apiv3.iucnredlist.org/api/v3/species/page/0?token=${apikey}`);
     const data = await response.json();
     const randomNumber=(Math.random() * 10000);
     h3.innerHTML = data.result[Math.trunc(randomNumber)].scientific_name;
